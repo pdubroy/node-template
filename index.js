7c7d06e8c6a805d8267fac0c5ca88a95f704d4a0
Patrick Dubroy <pdubroy@gmail.com>
2015-04-27 16:27:31 -0700

    Update copyright for 2015.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 Patrick Dubroy <pdubroy@gmail.com>
+// Copyright (c) 2015 Patrick Dubroy <pdubroy@gmail.com>
 // This software is distributed under the terms of the MIT License.
 
 'use strict';
